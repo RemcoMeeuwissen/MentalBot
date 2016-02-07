2d1720f0a5aeb58bc3b4096a4da5bb77b3850591
Remco Meeuwissen <hello@kalm.io>
2016-02-08 00:19:06 +0100

    Integrated the init.js options into main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('./mentalbot.sqlite')
 const rawjs = require('raw.js')
 const reddit = new rawjs(config['useragent'])
+const program = require('commander')
 
 const mentalbot = require('./mentalbot.js')(db, reddit, config)
 
@@ -14,18 +15,29 @@ reddit.setupOAuth2(
     config['secret']
 )
 
+program
+  .version('1.0.0')
+  .option('-r, --ignore-reddit', 'Do not post to Reddit')
+  .parse(process.argv)
+
 reddit.auth({'username': config['username'], 'password': config['password']}, (err, response) => {
     if(err) {
         console.log('Unable to authenticate user: ' + err);
     } else {
-        mentalbot.getRecentPosts().then((body) => {
+        mentalbot.createTable().then(() => {
+            return mentalbot.getRecentPosts()
+        }).then((body) => {
             return mentalbot.parseRecentPosts(body)
         }).then((data) => {
             let posts = data.map(mentalbot.savePost)
             return Promise.all(posts)
         }).then((data) => {
-            let newPosts = data.map(mentalbot.postToReddit)
-            return Promise.all(newPosts)
+            if (! program.ignoreReddit) {
+                let newPosts = data.map(mentalbot.postToReddit)
+                return Promise.all(newPosts)
+            } else {
+                return true
+            }
         }).then(() => {
             db.close()
             reddit.logout()
