376903965116934c5eea8053ad43f36003103624
Remco Meeuwissen <hello@kalm.io>
2016-02-08 13:09:31 +0100

    Fixed linting issues

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
-"use strict";
+'use strict';
 
-const config = require('./config.json')
+const config = require('./config.json');
 
-const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database('./mentalbot.sqlite')
-const rawjs = require('raw.js')
-const reddit = new rawjs(config['useragent'])
-const program = require('commander')
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./mentalbot.sqlite');
+const Rawjs = require('raw.js');
+const reddit = new Rawjs(config.useragent);
+const program = require('commander');
 
-const mentalbot = require('./mentalbot.js')(db, reddit, config)
+const mentalbot = require('./mentalbot.js')(db, reddit, config);
 
 reddit.setupOAuth2(
-    config['clientID'],
-    config['secret']
-)
+  config.clientID,
+  config.secret
+);
 
 program
   .version('1.0.0')
   .option('-r, --ignore-reddit', 'Do not post to Reddit')
-  .parse(process.argv)
+  .parse(process.argv);
 
-reddit.auth({'username': config['username'], 'password': config['password']}, (err, response) => {
-    if(err) {
-        console.log('Unable to authenticate user: ' + err);
-    } else {
-        mentalbot.createTable().then(() => {
-            return mentalbot.getRecentPosts()
-        }).then((body) => {
-            return mentalbot.parseRecentPosts(body)
-        }).then((data) => {
-            let posts = data.map(mentalbot.savePost)
-            return Promise.all(posts)
-        }).then((data) => {
-            if (! program.ignoreReddit) {
-                let newPosts = data.map(mentalbot.postToReddit)
-                return Promise.all(newPosts)
-            } else {
-                return true
-            }
-        }).then(() => {
-            db.close()
-            reddit.logout()
-        })
-    }
-})
+reddit.auth({ username: config.username, password: config.password }, (error) => {
+  if (error) {
+    console.log(`Unable to authenticate user: ${ error }`);
+  } else {
+    mentalbot.createTable().then(
+      () => mentalbot.getRecentPosts()
+    ).then(
+      (body) => mentalbot.parseRecentPosts(body)
+    ).then((data) => {
+      const posts = data.map(mentalbot.savePost);
+      return Promise.all(posts);
+    }).then((data) => {
+      if (program.ignoreReddit) return true;
+
+      const newPosts = data.map(mentalbot.postToReddit);
+      return Promise.all(newPosts);
+    }).then(() => {
+      db.close();
+      reddit.logout();
+    });
+  }
+});
--- mentalbot.js
+++ mentalbot.js
@@ -1,98 +1,83 @@
-"use strict";
+'use strict';
 
-const request = require('request')
-const cheerio = require('cheerio')
+const request = require('request');
+const cheerio = require('cheerio');
 
-module.exports = (db, reddit, config) => {
-    return {
-        'createTable': () => {
-            return new Promise((resolve, reject) => {
-                db.run('CREATE TABLE IF NOT EXISTS posts (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, url TEXT NOT NULL)', (error) => {
-                    if (error) {
-                        console.log(error)
-                    }
+module.exports = (db, reddit, config) => ({
+  createTable: () => new Promise((resolve) => {
+    db.run(`CREATE TABLE IF NOT EXISTS posts (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  url TEXT NOT NULL)`, (error) => {
+      if (error) console.log(error);
+      resolve();
+    });
+  }),
+  getRecentPosts: () => new Promise((resolve) => {
+    request('http://mentalpod.com/episodes', (error, response, body) => {
+      if (error || response.statusCode !== 200) console.log(error);
+      resolve(body);
+    });
+  }),
+  parseRecentPosts: (body) => new Promise((resolve) => {
+    const $ = cheerio.load(body);
+    const posts = [];
 
-                    resolve()
-                })
-            })
-        },
-        'getRecentPosts': () => {
-            return new Promise((resolve, reject) => {
-                request('http://mentalpod.com/episodes', (error, response, body) => {
-                    if (error || response.statusCode !== 200) {
-                        console.log(error)
-                    }
+    $('.widget_recent_entries li').each((i, elem) => {
+      const children = $(elem).children();
 
-                    resolve(body)
-                })
-            })
-        },
-        'parseRecentPosts': (body) => {
-            return new Promise((resolve, reject) => {
-                let $ = cheerio.load(body)
-                let posts = []
+      for (let x = 0; x < children.length; x++) {
+        const child = children[x];
+        if (child.tagName === 'a') posts.push([$(child).text(), $(child).attr('href')]);
+      }
+    });
 
-                $('.widget_recent_entries li').each((i, elem) => {
-                    let children = $(elem).children()
+    resolve(posts);
+  }),
+  savePost: (data) => {
+    const title = data[0];
+    const url = data[1];
 
-                    for (let i = 0; i < children.length; i++) {
-                        let child = children[i]
-
-                        if (child.tagName === 'a') {
-                            posts.push([$(child).text(), $(child).attr('href')])
-                        }
-                    }
-                })
-
-                resolve(posts)
-            })
-        },
-        'savePost': (data) => {
-            let title = data[0]
-            let url = data[1]
-
-            return new Promise((resolve, reject) => {
-                db.get('SELECT * FROM posts WHERE url = ?', [url], (err, row) => {
-                    if (err !== null) {
-                        console.log(err)
-                        resolve([])
-                    } else if (row === undefined) {
-                        db.run('INSERT INTO posts (title, url) VALUES (?, ?)', [title, url], (error) => {
-                            if (error !== null) {
-                                console.log(error)
-                            }
-
-                            resolve([title, url])
-                        })
-                    } else {
-                        resolve([])
-                    }
-                })
-            })
-        },
-        'postToReddit': (data) => {
-            if (data.length === 0) {
-                return true
-            } else {
-                let title = data[0]
-                let url = data[1]
-
-                return new Promise((resolve, reject) => {
-                    if (! url.startsWith('http://mentalpod.com/archives/')) {
-                        reddit.submit({'url': url, 'title': title, 'r': config['subreddit']}, (error, id) => {
-                            if (error !== null) {
-                                console.log(error)
-                            } else {
-                                console.log('Added ' + title + ' to Reddit')
-                            }
-
-                            resolve()
-                        })
-                    } else {
-                        resolve()
-                    }
-                })
-            }
+    return new Promise((resolve) => {
+      db.get('SELECT * FROM posts WHERE url = ?', [url], (err, row) => {
+        if (err !== null) {
+          console.log(err);
+          resolve([]);
         }
-    }
-}
+
+        if (err === null && row === undefined) {
+          db.run('INSERT INTO posts (title, url) VALUES (?, ?)', [title, url], (error) => {
+            if (error !== null) {
+              console.log(error);
+              resolve([]);
+            } else {
+              resolve([title, url]);
+            }
+          });
+        }
+      });
+    });
+  },
+  postToReddit: (data) => {
+    if (data.length === 0) return true;
+
+    const title = data[0];
+    const url = data[1];
+
+    return new Promise((resolve) => {
+      if (! url.startsWith('http://mentalpod.com/archives/')) {
+        reddit.submit({ url, title, r: config.subreddit }, (error) => {
+          if (error !== null) {
+            console.log(error);
+          } else {
+            console.log(`Added ${ title } to Reddit`);
+          }
+
+          resolve();
+        });
+      } else {
+        resolve();
+      }
+    });
+  },
+});
